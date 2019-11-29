f0abb64cb0b74e80aee4b718eb3d636d09e372a1
begeekmyfriend <begeekmyfriend@gmail.com>
2019-11-29 14:54:12 +0800

    Adapt for Rayhane Mamah's Tacotron-2
    Signed-off-by: begeekmyfriend <begeekmyfriend@gmail.com>

--- inference.py
+++ inference.py
@@ -3,8 +3,8 @@ import glob
 import tqdm
 import torch
 import argparse
+import numpy as np
 from scipy.io.wavfile import write
-
 from model.generator import Generator
 from utils.hparams import HParam, load_hparam_str
 
@@ -23,8 +23,8 @@ def main(args):
     model.eval(inference=False)
 
     with torch.no_grad():
-        for melpath in tqdm.tqdm(glob.glob(os.path.join(args.input_folder, '*.mel'))):
-            mel = torch.load(melpath)
+        for melpath in tqdm.tqdm(glob.glob(os.path.join(args.input_folder, '*.npy'))):
+            mel = torch.from_numpy(np.load(melpath))
             if len(mel.shape) == 2:
                 mel = mel.unsqueeze(0)
             mel = mel.cuda()
@@ -32,17 +32,17 @@ def main(args):
             audio = model.inference(mel)
             audio = audio.cpu().detach().numpy()
 
-            out_path = melpath.replace('.mel', '_reconstructed_epoch%04d.wav' % checkpoint['epoch'])
+            out_path = melpath.replace('.npy', '_reconstructed_epoch%04d.wav' % checkpoint['epoch'])
             write(out_path, hp.audio.sampling_rate, audio)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', type=str, default=None,
+    parser.add_argument('-c', '--config', type=str, default='config/default.yaml',
                         help="yaml file for config. will use hp_str from checkpoint if not given.")
     parser.add_argument('-p', '--checkpoint_path', type=str, required=True,
                         help="path of checkpoint pt file for evaluation")
-    parser.add_argument('-i', '--input_folder', type=str, required=True,
+    parser.add_argument('-i', '--input_folder', type=str, default='',
                         help="directory of mel-spectrograms to invert into raw audio. ")
     args = parser.parse_args()
 
